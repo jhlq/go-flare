e661c50ca93bc6bb7ec6c15b84d33ca85c2f5143
Marcus Appelros <marcus.appelros@gmail.com>
2021-01-03 00:54:20 +0100

    Create addressCmd.go

---
+++ cmd/addressCmd.go
@@ -0,0 +1,20 @@
+package cmd
+
+import (
+  "fmt"
+
+  "github.com/spf13/cobra"
+)
+
+func init() {
+  rootCmd.AddCommand(addressCmd)
+}
+
+var versionCmd = &cobra.Command{
+  Use:   "address",
+  Short: "Print the address associated with a secret key.",
+  Long:  `Print the address associated with a secret key or keystore.`,
+  Run: func(cmd *cobra.Command, args []string) {
+    fmt.Println("This will print the address")
+  },
+}
